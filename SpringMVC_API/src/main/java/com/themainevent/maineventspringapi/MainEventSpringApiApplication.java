cb3a4cf348f27c2ddbb80fa89a529cbb6d408080
rbrunney <rbrunney@student.neumont.edu>
2021-11-15 18:51:51 -0700

    Set-up Spring API

---
+++ SpringMVC_API/src/main/java/com/themainevent/maineventspringapi/MainEventSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.themainevent.maineventspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainEventSpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainEventSpringApiApplication.class, args);
+    }
+
+}
---
+++ SpringMVC_API/src/test/java/com/themainevent/maineventspringapi/MainEventSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.themainevent.maineventspringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainEventSpringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
