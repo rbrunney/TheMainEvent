f7fcc91a247761368fff0e8b40a1156e7a2b3430
rbrunney <rbrunney@student.neumont.edu>
2021-11-20 15:42:30 -0700

    Converting Plain HTML, CSS, JS to NodeJS

---
+++ TheMainEvent_Frontend/routes/routes.js
@@ -0,0 +1,15 @@
+exports.index = (req, res) => {
+    res.render('index')
+};
+
+exports.orderPage = (req, res) => {
+    res.render('orderPage');
+};
+
+exports.accountInfo = (req, res) => {
+    res.render('accountInfo');
+};
+
+exports.freezerMeals = (req, res) => {
+    res.render('freezerMeals')
+};
