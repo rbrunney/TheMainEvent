69b6916528cdb6f01c779e8bc05d9875510aa804
Dragonliver <castanley77@live.com>
2021-11-22 19:08:55 -0700

    Order Details CRUD done!

--- SpringMVC_API/src/main/java/com/themainevent/maineventspringapi/Controllers/BusinessLogicLayer/BLLOrderDetails.java
+++ SpringMVC_API/src/main/java/com/themainevent/maineventspringapi/Controllers/BusinessLogicLayer/BLLOrderDetails.java
@@ -1,9 +1,12 @@
 package com.themainevent.maineventspringapi.Controllers.BusinessLogicLayer;
 
+import com.themainevent.maineventspringapi.Models.ModelMenuItem;
 import com.themainevent.maineventspringapi.Models.ModelOrderDetails;
 import com.themainevent.maineventspringapi.Repositories.OrderDetailsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BLLOrderDetails {
@@ -18,6 +21,35 @@ public class BLLOrderDetails {
         } catch(Exception e) {
             e.printStackTrace();
             return "Order Details could not be saved";
+        }
+    }
+
+    //Find By Name
+    public List<ModelOrderDetails> getByFirstName(String name){
+        return orderDetailRepo.findByName(name);
+    }
+
+    //Delete
+    public String Delete(String name){
+        try {
+            orderDetailRepo.deleteByName(name);
+            return "Order Details has been deleted";
+        }catch (Exception e){
+            e.printStackTrace();
+            return "Order Details could not be Deleted";
+        }
+    }
+
+    //Update
+    public String updateOrderDetails(String name, String newName){
+        try {
+            List<ModelOrderDetails> orderDetails = orderDetailRepo.findByName(name);
+
+
+            return "Order Details has been Updated";
+        }catch (Exception e){
+            e.printStackTrace();
+            return "Order Details could not be Updated";
         }
     }
 }
--- SpringMVC_API/src/main/java/com/themainevent/maineventspringapi/Controllers/Mapping/OrderDetailsController.java
+++ SpringMVC_API/src/main/java/com/themainevent/maineventspringapi/Controllers/Mapping/OrderDetailsController.java
@@ -1,9 +1,13 @@
 package com.themainevent.maineventspringapi.Controllers.Mapping;
 
 import com.themainevent.maineventspringapi.Controllers.BusinessLogicLayer.BLLOrderDetails;
+import com.themainevent.maineventspringapi.Models.ModelInventory;
 import com.themainevent.maineventspringapi.Models.ModelOrderDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path="/orderDetails")
@@ -28,5 +32,21 @@ public class OrderDetailsController {
         System.out.println("[ORDER DETAILS INFO] Menu Items: " + orderDetails.getMenuItems());
 
         return bllOrderDetails.add(orderDetails);
+    }
+
+    @DeleteMapping(path = "/delete/{name}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public String delete(@PathVariable String name){return bllOrderDetails.Delete(name);}
+
+    @PutMapping(path = "/update/{oldName}/{newName}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public String update(@PathVariable String newName, @PathVariable String oldName){
+        return bllOrderDetails.updateOrderDetails(oldName, newName);
+    }
+
+    @GetMapping(path = "/search/{name}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public List<ModelOrderDetails> read(@PathVariable String name) {
+        return bllOrderDetails.getByFirstName(name);
     }
 }
