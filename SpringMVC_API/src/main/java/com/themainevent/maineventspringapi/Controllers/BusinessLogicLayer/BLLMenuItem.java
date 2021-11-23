449481a5a68f5f35cc16ae77bbde76581a9a5195
Dragonliver <castanley77@live.com>
2021-11-22 18:37:27 -0700

    Menu Item CRUD done

--- SpringMVC_API/src/main/java/com/themainevent/maineventspringapi/Controllers/BusinessLogicLayer/BLLMenuItem.java
+++ SpringMVC_API/src/main/java/com/themainevent/maineventspringapi/Controllers/BusinessLogicLayer/BLLMenuItem.java
@@ -1,9 +1,12 @@
 package com.themainevent.maineventspringapi.Controllers.BusinessLogicLayer;
 
+import com.themainevent.maineventspringapi.Models.ModelIngredient;
 import com.themainevent.maineventspringapi.Models.ModelMenuItem;
 import com.themainevent.maineventspringapi.Repositories.MenuItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BLLMenuItem {
@@ -19,6 +22,35 @@ public class BLLMenuItem {
         }catch(Exception e){
             e.printStackTrace();
             return "Could not be added to Menu Items.";
+        }
+    }
+
+    //Find By Name
+    public List<ModelMenuItem> getByFirstName(String name){
+        return menuItemRepo.findByName(name);
+    }
+
+    //Delete
+    public String Delete(String name){
+        try {
+            menuItemRepo.deleteByName(name);
+            return "Menu Item has been deleted";
+        }catch (Exception e){
+            e.printStackTrace();
+            return "Menu Item could not be Deleted";
+        }
+    }
+
+    //Update
+    public String updateMenuItem(String name, String newName){
+        try {
+            List<ModelMenuItem> menuItem = menuItemRepo.findByName(name);
+
+
+            return "Menu Item has been Updated";
+        }catch (Exception e){
+            e.printStackTrace();
+            return "Menu Item could not be Updated";
         }
     }
 }
