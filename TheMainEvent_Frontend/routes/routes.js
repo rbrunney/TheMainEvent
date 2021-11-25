5bd109c4635b5c7f41c3a14a4f3752a193229d8e
rbrunney <rbrunney@student.neumont.edu>
2021-11-24 19:00:19 -0700

    Testing a connection to Spring Boot

--- TheMainEvent_Frontend/routes/routes.js
+++ TheMainEvent_Frontend/routes/routes.js
@@ -34,12 +34,18 @@ exports.addAccount = (req, res) => {
             username: bcrypt.hashSync(req.body.username, bcrypt.genSaltSync(10)),
             password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10)),
         }
-
         // Need to call REST API here so we can add their information to the Database
-        
-
+        const request = new XMLHttpRequest();
+        request.open('POST', 'localhost:8082/user/add');
+        request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        request.send(JSON.stringify(user));
+        request.onload = () => {
+            alert("[INFO] " + request.responseText)
+        }
+    
         res.redirect('/');
     } else {
         res.redirect('/createAccount');
     }
+
 }
